1cdacc2571ce85b1a4802c0fdf08a524d3dc698e
manu <manu.softwareengineer@gmail.com>
2024-04-26 12:10:56 +0000

    updated the endpoint

--- llm/api.py
+++ llm/api.py
@@ -3,66 +3,36 @@ from pydantic import BaseModel
 from chatbot import get_response
 from model import model 
 import pandas as pd
+from dotenv import load_dotenv
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv()
 app = FastAPI()
-
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5501"], # Allow requests from your frontend's origin
+    allow_credentials=True,
+    allow_methods=["*"], # Allow all methods
+    allow_headers=["*"], # Allow all headers
+)
 class Message(BaseModel):
     text: str
+    context: str
+    session_id: str
 
-
-
-@app.post('/check')
-async def check_health_data(
-    response: Response,
-    age: int = Form(...),
-    sex: int = Form(...),
-    cp: int = Form(...),
-    trestbps: int = Form(...),
-    chol: int = Form(...),
-    fbs: int = Form(...),
-    restecg: int = Form(...),
-    thalach: int = Form(...),
-    exang: int = Form(...),
-    oldpeak: float = Form(...),
-    slope: int = Form(...),
-    ca: int = Form(...),
-    thal: int = Form(...)
-):
-    new_data = pd.DataFrame({
-        "age": [age],
-        "sex": [sex],
-        "cp": [cp],
-        "trestbps": [trestbps],
-        "chol": [chol],
-        "fbs": [fbs],
-        "restecg": [restecg],
-        "thalach": [thalach],
-        "exang": [exang],
-        "oldpeak": [oldpeak],
-        "slope": [slope],
-        "ca": [ca],
-        "thal": [thal],
-    }, index=[0])
-
-    answer = model.predict(new_data)
-    if answer == 1:
-        response_text = "Status: You have heart disease"
-    elif answer == 0:
-        response_text = "Status: You not have heart disease"
-    
-    response.set_cookie(key="health_status", value=response_text)
-    return {"response": response_text}
+port = int(os.getenv("PORT", 9000))
 
 
 @app.post("/chat")
-async def chat_with_bot(message: Message, request: Request):
-    health_status = request.cookies.get("health_status")
-    if health_status:
-        context = health_status
-    else:
-        context = "No health status found"
-    
-    output = get_response(context=context, message=message.text)
-    response= output.split('\n')
-    return {"response": response[0:2]}
+async def chat_with_bot(message: Message):
+    session_id = message.get("session_id")
+    context=message.get("context")
+    text=message.get("text")
+    output = get_response(context=context,message=text, session_id=session_id)
+    return {"response": output}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
--- llm/chatbot.py
+++ llm/chatbot.py
@@ -1,89 +1,90 @@
-import chromadb,os
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
-from langchain_community.embeddings.sentence_transformer import (
-    SentenceTransformerEmbeddings,
-)
-from langchain_community.vectorstores import Chroma
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA, LLMChain
-from sentence_transformers import SentenceTransformer
-from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
+import os
+
 
 load_dotenv()
 
-# llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key="os.getenv("OPENAI_API_KEY")")
+model =  ChatOpenAI(
+        api_key= os.getenv("OPENAI_API_KEY"),
+        temperature=0.2,
+        model='gpt-3.5-turbo-1106'
+    )
 
-llm = HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
-    task="text-generation",
-    repetition_penalty= 1.03,
-    verbose=False,
-    max_new_tokens= 512,
-    top_k= 30,
-    temperature= 0.4,
-    huggingfacehub_api_token= "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",
-    
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are Heartty, a cardiologist, provide accurate answers based on the {context} provided. 
+            If you don't know the answer to any question truthfully say so and do not hallucinate.""",
+        ),
+        MessagesPlaceholder(variable_name="chathistory"),
+        ("human", "{input}"),
+    ]
+)
+chain = prompt | model
+
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversation = RunnableWithMessageHistory(
+    chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chathistory",
+   
 )
 
-template = """
-Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
-Always politely request if they would like to know more or anything else they want to know.
+# results = "Status: You have heart disease"
+# context = f""""
+# base on this your details;
+#         "age": 57,
+#         "sex": 1,
+#         "cp": 2,
+#         "trestbps": 156,
+#         "chololestrol": 300,
+#         "fbs": 1,
+#         "restecg": 0,
+#         "thalach": 156,
+#         "exang": 1,
+#         "oldpeak": 3.2,
+#         "slope": 0,
+#         "ca": 0,
+#         "thal": 3
+#         {results}
+#     """
 
-{context}
 
-Question: {question}
 
-Helpful Answer:
-"""
-
-# context = """
-# Name: John Doe
-# Age: 55
-# Sex: Male
-# """
-"""
-"age": 70,
-    "sex": 1,
-    "cp": 0,
-    "trestbps": 145,
-    "chol": 174,
-    "fbs": 0,
-    "restecg": 1,
-    "thalach": 125,
-    "exang": 1,
-    "oldpeak": 2.6,
-    "slope": 0,
-    "ca": 0,
-    "thal": 3
-  },index=[0])
-
-  "age": 53,
-    "sex": 1,
-    "cp": 0,
-    "trestbps": 140,
-    "chol": 203,
-    "fbs": 1,
-    "restecg": 0,
-    "thalach": 155,
-    "exang": 1,
-    "oldpeak": 3.1,
-    "slope": 0,
-    "ca": 0,
-    "thal": 3
-"""
-
-prompt = PromptTemplate(
-        input_variables=["context", "question"], template=template
+def get_response(context, message, session_id):
+    response = conversation.invoke(
+        {"context": context, "input": message},
+        config={"configurable": {"session_id": session_id}},
     )
-chain = LLMChain(llm=llm, prompt=prompt)
 
+    return response.content
 
-def get_response(context, message):
-    response = chain.invoke(input={"context":context,'question':message})
-    return response['text']
+# print(get_response(context=context, message="what is my status", session_id=345))
+
+# while True:
+#     user_input = input("User :")
+#     if user_input.lower() == 'exit':
+#             break
+#     response = conversation.invoke(
+#         {"context": context, "input": user_input},
+#         config={"configurable": {"session_id": "abc123"}},
+#     )
+
+#     print(response.content)
+
 
