7ee43c24241ea54879e14a484118ada7fda2fc43
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-04-26 14:44:36 +0000

    new frontend

---
+++ frontend/src/pages/chatbot/ChatThread.tsx
@@ -0,0 +1,16 @@
+const ChatThread = () => {
+  return (
+    <div className="w-full flex flex-col gap-5">
+      <div className="px-4 py-6 rounded-md w-[60%] max-w-[60%] bg-[rgba(178,178,238,0.4)]">
+        hello
+      </div>
+      <div className="px-4 py-6 rounded-md w-[30%] max-w-[60%] bg-[rgba(178,178,238,0.1)]">
+        hello
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ChatThread;
---
+++ frontend/src/utils/routes.ts
@@ -0,0 +1,7 @@
+export const HOME_PAGE = "/";
+export const LOGIN = "/login";
+export const REGISTER = "/register";
+export const ADMIN = "/admin";
+export const PATIENT = "/patient";
+export const PAGE_NOT_FOUND = "*";
+export const CHAT_BOT = "/chatbot";
