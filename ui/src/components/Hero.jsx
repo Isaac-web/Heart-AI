04164c19de6677e4de02c38b577a1bd8c0ab0531
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-04-26 01:27:52 +0000

    landing page

---
+++ ui/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+
+const Hero = () => {
+    return (
+        <>
+            <section className="px-5 mt-10">
+                <h2 className="font-bold text-2xl pb-3">Healthcare, at your call.</h2>
+                <p className="text-[#83869A] pb-7">Welcome! This app helps you understand your heart health by estimating your risk of heart disease.</p>
+                <button className="bg-gradient-to-r from-[#FF6F48] to-[#F02AA6] text-white px-6 py-2 rounded-full">Get Started</button>
+            </section>
+        </>
+    )
+}
+
+export default Hero
