d98274aee69cc1956486d0c4072e6e7160fc26db
Isaac-web <kanytakiy@gmail.com>
2024-04-26 04:30:24 +0000

    Set up Db connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import debug from 'debug';
 import config from 'config';
 
 const app = express();
+const logDb = debug('db');
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Here we go!' });
@@ -9,6 +12,14 @@ app.get('/', (req: Request, res: Response) => {
 
 const port = config.get('port');
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
-});
+mongoose
+  .connect(config.get('db.url'))
+  .then(() => {
+    logDb(`Connected to ${config.get('db.url')}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}...`);
+    });
+  })
+  .catch((err) => {
+    console.error('Something went wrong while connecting to mongodb.', err);
+  });
