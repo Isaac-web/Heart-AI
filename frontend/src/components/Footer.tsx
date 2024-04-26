720a89c781ff06879284a06bc4703aea09d0187b
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-04-26 15:19:23 +0000

    added components and pages

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="mt-auto flex justify-between px-5 p-8 bg-[#13183F]">
+        <h1 className="text-white mt-2 font-bold text-xl">HeartAI</h1>
+        <Link
+          className="bg-gradient-to-r from-[#4851FF] to-[#F02AA6] rounded-full text-white px-4 py-2"
+          to="/auth"
+        >
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
