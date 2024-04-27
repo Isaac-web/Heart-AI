cd6bba62e4df7ae1927a8ac9029839c5bfd15c25
manu <manu.softwareengineer@gmail.com>
2024-04-27 16:33:45 +0000

    connected it to mongodb

--- chatbot.py
+++ chatbot.py
@@ -1,69 +1,48 @@
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder,HumanMessagePromptTemplate
 from langchain_openai.chat_models import ChatOpenAI
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
+import os 
 from dotenv import load_dotenv
-import os
-
 
 load_dotenv()
 
+
 model =  ChatOpenAI(
-        api_key= os.getenv("OPENAI_API_KEY"),
+        api_key= os.getenv("OPENAI-API_KEY"),
         temperature=0.2,
         model='gpt-3.5-turbo-1106'
     )
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        (
+    
+             (
             "system",
-            """You are Heartty, a cardiologist, provide accurate answers based on the {context} provided. 
+            """You are hearty a cardiologist, provide accurate answers based on the {context} provided.
+            You should pay particular english semantics. 
             If you don't know the answer to any question truthfully say so and do not hallucinate.""",
         ),
+            
         MessagesPlaceholder(variable_name="chathistory"),
-        ("human", "{input}"),
+        HumanMessagePromptTemplate.from_template(
+            "{input}"
+        ),
     ]
 )
 chain = prompt | model
 
-store = {}
-
-
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = ChatMessageHistory()
-    return store[session_id]
-
 
 conversation = RunnableWithMessageHistory(
     chain,
-    get_session_history,
+    lambda session_id: MongoDBChatMessageHistory(
+        session_id=session_id,
+        connection_string=os.getenv("MONGO_URI"),
+        database_name=os.getenv("MONGO_INITDB_DATABASE"),
+        collection_name=os.getenv("COLLECTION"), ),
     input_messages_key="input",
     history_messages_key="chathistory",
-   
 )
-
-# results = "Status: You have heart disease"
-# context = f""""
-# base on this your details;
-#         "age": 57,
-#         "sex": 1,
-#         "cp": 2,
-#         "trestbps": 156,
-#         "chololestrol": 300,
-#         "fbs": 1,
-#         "restecg": 0,
-#         "thalach": 156,
-#         "exang": 1,
-#         "oldpeak": 3.2,
-#         "slope": 0,
-#         "ca": 0,
-#         "thal": 3
-#         {results}
-#     """
-
 
 
 def get_response(context, message, session_id):
@@ -73,18 +52,4 @@ def get_response(context, message, session_id):
     )
 
     return response.content
-
-# print(get_response(context=context, message="what is my status", session_id=345))
-
-# while True:
-#     user_input = input("User :")
-#     if user_input.lower() == 'exit':
-#             break
-#     response = conversation.invoke(
-#         {"context": context, "input": user_input},
-#         config={"configurable": {"session_id": "abc123"}},
-#     )
-
-#     print(response.content)
-
 
