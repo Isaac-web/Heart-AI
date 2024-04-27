49243832d2aa869fcf7a20b51a9814e3c3176965
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-04-27 21:14:38 +0000

    dashboard

---
+++ frontend/src/components/Results.tsx
@@ -0,0 +1,19 @@
+const Results = () => {
+  return (
+    <>
+      
+      <div className="ml-3 mt-8">
+        <h2 className="text-[#F74780] font-bold text-2xl">Hi, John</h2>
+        <p className="text-[#13183F] mt-5">Heart Results Summary</p>
+      </div>
+      <div className="bg-[#F3F4FF] p-4 mt-1 md:max-w-xl">
+        <div className="mt-4">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>Lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Results;
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,17 +2,19 @@ import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Features from "../../components/Features";
 import Footer from "../../components/Footer";
+import Dashboard from "../Dashboard";
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-grow">
-        <Header />
-        <Hero />
-        <Features />
-      </div>
-      <Footer />
-    </div>
+    <Dashboard/>
+    // <div className="flex flex-col min-h-screen">
+    //   <div className="flex-grow">
+    //     <Header />
+    //     <Hero />
+    //     <Features />
+    //   </div>
+    //   <Footer />
+    // </div>
   );
 };
 
