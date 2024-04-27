b177bd7e0cdf6fe2f37917fb70fd09f59bfb5ec7
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-04-27 21:15:26 +0000

    dashboard

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,19 +2,17 @@ import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Features from "../../components/Features";
 import Footer from "../../components/Footer";
-import Dashboard from "../Dashboard";
 
 const Home = () => {
   return (
-    <Dashboard/>
-    // <div className="flex flex-col min-h-screen">
-    //   <div className="flex-grow">
-    //     <Header />
-    //     <Hero />
-    //     <Features />
-    //   </div>
-    //   <Footer />
-    // </div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Header />
+        <Hero />
+        <Features />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
