f0f1ed64cf639242a8477bbb9c72136fbcff8fbe
Isaac-web <kanytakiy@gmail.com>
2024-04-28 17:46:23 +0000

    Add Medical Report Model

--- backend/src/models/MedicalReport.ts
+++ backend/src/models/MedicalReport.ts
@@ -1,18 +1,100 @@
-import { Schema, model, Document } from 'mongoose';
-import { Types } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-export interface IMedicalReport extends Document {
-  id: Types.ObjectId;
-  doctorId: Types.ObjectId;
-  patientId: Types.ObjectId;
-  createdAt: Date;
-}
-
-const MedicalReportSchema: Schema = new Schema<IMedicalReport>({
-  id: { type: Schema.Types.ObjectId, auto: true },
+const MedicalReportSchema: Schema = new Schema({
   doctorId: { type: Schema.Types.ObjectId, required: true },
   patientId: { type: Schema.Types.ObjectId, required: true },
-  createdAt: [{ type: Date, required: true }],
+  age: {
+    type: Number,
+    min: 0,
+    max: 100,
+    required: true,
+  },
+  sex: {
+    type: String,
+    minlength: 3,
+    maxlength: 10,
+    required: true,
+  },
+  trestbps: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  chol: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  fbs: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  thalach: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  exang: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  oldpeak: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  slope: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  ca: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  cp_1: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  cp_2: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  cp_3: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  restecg_1: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  restecg_2: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  thal_1: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  thal_2: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  thal_3: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
 });
 
-export default model<IMedicalReport>('MedicalReport', MedicalReportSchema);
+export default model('MedicalReport', MedicalReportSchema);
