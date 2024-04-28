5e45eafa1422218a23e1874a9070daee5b2397cf
Isaac-web <kanytakiy@gmail.com>
2024-04-28 00:10:32 +0000

    Add Formik To Login Screen

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -6,54 +6,90 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email().required().label('Email'),
+  password: Yup.string().min(7).max(256).required().label('Password'),
+});
 
 const Login = () => {
   const theme = useTheme();
   return (
-    <Grid
-      container
-      justifyContent={'center'}
-      alignContent={'center'}
-      sx={{
-        height: '100vh',
-        width: '100%',
-        pl: theme.spacing(2),
-        pr: theme.spacing(2),
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={{
+        email: '',
+        password: '',
       }}
+      onSubmit={(data) => console.log(data)}
     >
-      <Grid item xs={12} md={8} xl={4}>
-        <Paper
+      {({ handleChange, handleSubmit, errors, touched }) => (
+        <Grid
+          container
+          justifyContent={'center'}
+          alignContent={'center'}
           sx={{
-            pt: theme.spacing(4),
-            pb: theme.spacing(4),
-            pl: theme.spacing(5),
-            pr: theme.spacing(5),
+            height: '100vh',
+            width: '100%',
+            pl: theme.spacing(2),
+            pr: theme.spacing(2),
           }}
         >
-          <Grid container direction="column" spacing={2}>
-            <Grid item>
-              <Grid container justifyContent={'center'} alignItems={'center'}>
-                <Typography variant="h5">Login</Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <TextField label="Email" />
-            </Grid>
-            <Grid item>
-              <Grid item>
-                <TextField label="Password" />
-              </Grid>
-            </Grid>
+          <Grid item xs={12} md={8} xl={4}>
+            <Paper
+              sx={{
+                pt: theme.spacing(4),
+                pb: theme.spacing(4),
+                pl: theme.spacing(5),
+                pr: theme.spacing(5),
+              }}
+            >
+              <Grid container direction="column" spacing={2}>
+                <Grid item>
+                  <Grid
+                    container
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                  >
+                    <Typography variant="h5">Login</Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <TextField
+                    label="Email"
+                    onChange={handleChange('email')}
+                    helperText={touched['email'] && errors['email']}
+                    error={Boolean(touched['email'] && errors['email'])}
+                  />
+                </Grid>
+                <Grid item>
+                  <Grid item>
+                    <TextField
+                      label="Password"
+                      onChange={handleChange('password')}
+                      helperText={touched['password'] && errors['password']}
+                      error={Boolean(touched['password'] && errors['password'])}
+                    />
+                  </Grid>
+                </Grid>
 
-            <Grid item>
-              <Button size="large" variant="contained">
-                Login
-              </Button>
-            </Grid>
+                <Grid item>
+                  <Button
+                    size="large"
+                    variant="contained"
+                    onClick={() => handleSubmit()}
+                  >
+                    Login
+                  </Button>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
-        </Paper>
-      </Grid>
-    </Grid>
+        </Grid>
+      )}
+    </Formik>
   );
 };
 
