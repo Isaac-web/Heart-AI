e965a46864e1572ccdec62afa4b38e4ffb2e578c
Isaac-web <kanytakiy@gmail.com>
2024-04-29 18:28:03 +0000

    Implement fetching of MedicalReports and Medical Report Requests

--- backend/src/routes/medicalReportRequest.routes.ts
+++ backend/src/routes/medicalReportRequest.routes.ts
@@ -3,13 +3,14 @@ import {
   createMedicalReportRequest,
   deleteMedicalReportRequest,
   fetchMedicalReportRequest,
+  fetchUserMedicalReport,
 } from '../controllers/medicalReportRequest.controllers';
 import { auth } from '../middleware/auth';
 
 const router = Router();
 
 router.post('/', auth, createMedicalReportRequest);
-router.get('/me', auth, async (req, res) => {});
+router.get('/me', auth, fetchUserMedicalReport);
 router.get('/', auth, fetchMedicalReportRequest);
 router.delete('/:id', deleteMedicalReportRequest);
 
