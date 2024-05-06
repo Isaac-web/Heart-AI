806d94261efc6d4b6ac69e31d4914d1e4ba30858
takiy-bdg <isaac-web.bigdataghana@outlook.com>
2024-05-06 14:20:43 +0000

    Add Login and Register Screens for Doctors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,91 +1,22 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-  useNavigate,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DoctorSignUpPage from './pages/DoctorSignUpPage';
+import DoctorOnboardPerfonalInforPage from './pages/DoctorOnboardPerfonalInforPage';
 
-import { paths } from '@/utils/routes';
-import { AppContextProvider } from '@/contexts/AppContext';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Doctor from './pages/doctor/Doctor';
-import MedicalReportForm from './pages/MedicalReportForm';
-import Home from './pages/home/Home';
-import { useEffect } from 'react';
-import { getUserId } from './utils/auth';
-import { getCurrentUser } from './api/auth';
-import Header from './components/Header';
-import Chatbot from './pages/chatbot/Chatbot';
-import { useAppStore } from './hooks/store';
-
-const DoctorsPortal = () => {
-  const navigate = useNavigate();
-
-  const ensureUserIsDoctor = async () => {
-    if (!getUserId()) navigate('/login');
-
-    const user = await getCurrentUser();
-    if (user.userType !== 'doctor') navigate('/login');
-  };
-
-  useEffect(() => {
-    ensureUserIsDoctor();
-  }, []);
-
-  return <Outlet />;
-};
-
-const PatientPortal = () => {
-  const navigate = useNavigate();
-
-  const ensureUserIsDoctor = async () => {
-    if (!getUserId()) navigate('/login');
-
-    const user = await getCurrentUser();
-    if (user.userType == 'patient') navigate('/chatbot');
-  };
-
-  useEffect(() => {
-    ensureUserIsDoctor();
-  }, []);
-
-  return <Outlet />;
-};
-
-export default function App() {
-  const store = useAppStore();
-
-  useEffect(() => {
-    if (getUserId()) {
-      store.getCurrentUser();
-    }
-  }, []);
-
+const App = () => {
   return (
-    <AppContextProvider>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path={paths.HOME_PAGE} element={<Home />} />
-          <Route path={paths.LOGIN} element={<Login />} />
-          <Route path={paths.REGISTER} element={<Register />} />
-          <Route path={`${paths.CHAT_BOT}/:sessionId`} element={<Chatbot />} />
-
-          {/* <Route path={paths.CHAT_BOT} element={<PatientPortal />}> */}
-          <Route path={paths.CHAT_BOT} element={<Chatbot />} />
-          {/* </Route> */}
-
-          <Route path={paths.DOCTOR} element={<DoctorsPortal />}>
-            <Route path={paths.DOCTOR} element={<Doctor />} />
-            <Route
-              path={paths.MEDICAL_REPORT_FORM}
-              element={<MedicalReportForm />}
-            />
-          </Route>
-        </Routes>
-      </Router>
-    </AppContextProvider>
+    <main>
+      {/* <nav>Logo</nav> */}
+      <Routes>
+        <Route
+          path="/doctor/onboarding"
+          element={<DoctorOnboardPerfonalInforPage />}
+        />
+        <Route path="/register/doctor" element={<DoctorSignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </main>
   );
-}
+};
+
+export default App;
