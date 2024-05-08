0613deaa1710af392d4bb0caf8341bf5e54f98cd
takiy-bdg <isaac-web.bigdataghana@outlook.com>
2024-05-08 09:49:12 +0000

    Refactor Store

---
+++ frontend/src/store/types/index.ts
@@ -0,0 +1,97 @@
+import {
+  ChatMessage,
+  ChatMessageFormData,
+  ChatSession,
+  LoginFormData,
+  MedicalReport,
+  MedicalReportFormData,
+  MedicalReportRequest,
+  MedicalReportSearchParams,
+  RegistrationFormData,
+  User,
+  UsersSearchParams,
+} from '@/types';
+
+interface RequestError {
+  callingFunction: string;
+  message: string;
+}
+
+export interface RequestErrorState {
+  errors: RequestError[];
+  addError(error: RequestError): void;
+  removeError(callingFunctionName: string): void;
+  getError(callingFunctionName: string): RequestError | null;
+}
+
+export interface AuthSlice {
+  authPending: boolean;
+  loadingCurrentUser: boolean;
+  currentUser: User | null;
+  login(data: LoginFormData): Promise<void>;
+  getCurrentUser(): Promise<void>;
+  register(data: RegistrationFormData): Promise<void>;
+}
+
+export interface UsersSlice {
+  users: User[];
+  fetchUsers(params?: UsersSearchParams): Promise<void>;
+}
+
+export interface MedicalReportRequestSlice {
+  medicalReportRequests: MedicalReportRequest[];
+  loadingMedicalReportRequests: boolean;
+  fetchMedicalReportRequests(): Promise<void>;
+}
+
+export interface MedicalReportSlice {
+  medicalReports: MedicalReport[];
+  creatingMedicalReport: boolean;
+  loadingMedicalReports: boolean;
+  fetchMedicalReports(params: MedicalReportSearchParams): Promise<void>;
+  createMedicalReport(data: MedicalReportFormData): Promise<void>;
+  getCurrentUserMedicalReports(): Promise<void>;
+}
+
+export interface ChatSessionSlice {
+  chatSessions: ChatSession[];
+  creatingChatSession: boolean;
+  loadingChatSession: boolean;
+  deletingChatSession: boolean;
+  fetchChatSessions(): Promise<void>;
+  createChatSession(data: { title: string }): Promise<void>;
+  deleteChatSession(sessionId: string): Promise<void>;
+}
+
+export interface ChatMessagesSlice {
+  chatMessages: ChatMessage[];
+  creatingChatMessages: boolean;
+  loadingChatMessages: boolean;
+  fetchChatMessages(sessionId: string): Promise<void>;
+  sendChatMessage(data: ChatMessageFormData): Promise<void>;
+}
+
+export interface NamesEntity {
+  loading: boolean;
+  isPending: boolean;
+  total: number;
+  skip: number;
+  limit: number;
+  data: string[];
+  addData(name: string): void;
+}
+
+export interface AppDataSlice {
+  entities: {
+    names: NamesEntity;
+  };
+}
+
+export type StoreState = RequestErrorState &
+  AuthSlice &
+  UsersSlice &
+  MedicalReportRequestSlice &
+  MedicalReportSlice &
+  ChatSessionSlice &
+  ChatMessagesSlice &
+  AppDataSlice;
