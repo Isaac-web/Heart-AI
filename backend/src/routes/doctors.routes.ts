394441019f7255504758bd89d12e11f637554708
takiy-bdg <isaac-web.bigdataghana@outlook.com>
2024-05-09 00:18:46 +0000

    Code clean up

--- backend/src/routes/doctors.routes.ts
+++ backend/src/routes/doctors.routes.ts
@@ -10,10 +10,9 @@ import { doctorAuth } from '../middleware/doctorAuth';
 const router = Router();
 
 router.post('/register', registerDoctor);
-
 router.post('/login', doctorLogin);
-
 router.get('/me', doctorAuth, getCurrentDoctor);
+router.patch('/:id', doctorAuth, updateDoctor);
 
 router.get('/:id', (req, res) => {
   res.json({ message: 'Get Doctor By Id' });
@@ -22,8 +21,6 @@ router.get('/:id', (req, res) => {
 router.get('/', (req, res) => {
   res.json({ message: 'Fetch All Doctors' });
 });
-
-router.patch('/:id', doctorAuth, updateDoctor);
 
 router.patch('/:id', (req, res) => {
   res.json({ message: 'Doctors endpoint' });
