68de298e21426cd2ca49a892e05bab8eb6483f62
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-05-14 11:37:36 +0000

    reports-page

---
+++ frontend/src/components/Icons.tsx
@@ -0,0 +1,53 @@
+export const DownloadIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M12 22v-9m0 9l-2.5-2m2.5 2l2.5-2" />
+    <path d="M5.034 9.117A4.002 4.002 0 0 0 6 17h1" />
+    <path d="M15.83 7.138a5.5 5.5 0 0 0-10.796 1.98S5.187 10 5.5 10.5" />
+    <path d="M17 17a5 5 0 1 0-1.17-9.862L14.5 7.5" />
+  </svg>
+);
+
+export const FullScreenIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M2 7V2h5" />
+    <path d="M22 7V2h-5" />
+    <path d="M7 22H2v-5" />
+    <path d="M17 22h5v-5" />
+  </svg>
+);
+
+export const ShareIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M22 11l-7-9v5C3.047 7 1.668 16.678 2 22c.502-2.685.735-7 13-7v5l7-9z" />
+  </svg>
+);
