36e430d5e7069bcd4696f2257e91b6bfbf4c20ab
Isaac-web <kanytakiy@gmail.com>
2024-09-10 15:15:41 +0000

    Add a maintenance screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,95 +20,105 @@ import Auth from './pages/auth/AuthPage';
 import PatientsPage from './pages/PatientsPage';
 import PatientReportDetailsPage from './pages/PatientReportDetailsPage';
 import { useEffect } from 'react';
+import MaintenanceScreen from './components/MaintenanceScreen';
 
 const App = () => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [location.pathname])
-
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
-    <main>
-      <Routes>
-        <Route index element={<LandingPage />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
-        <Route path="/dashboard/patient" element={<PatientDashboard />}>
-          <Route path="chatbot/:sessionId" element={<Chatbot />} />
-          <Route path="chatbot" element={<Chatbot />} />
-          <Route path="reports" element={<Reports />} />
-          <Route
-            path="/dashboard/patient"
-            element={<Navigate to="/dashboard/patient/chatbot" />}
-          />
-        </Route>
+    <>
+      <MaintenanceScreen />
 
-        <Route path="/doctor/onboarding" element={<DoctorOnboardingPage />} />
-        <Route path="/onboarding/doctor" element={<DoctorOnboardingPage />} />
-        <Route path="/onboarding/patient" element={<PatientOnboardingPage />} />
-        <Route path="/register/doctor" element={<SignUpPage />} />
-        <Route path="/register/patient" element={<SignUpPage />} />
-        <Route path="/login/doctor" element={<LoginPage />} />
-        <Route path="/login/patient" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/portal" element={<PortalContainer />}>
-          <Route path="/portal/patient" element={<PatientDashboard />}>
-            <Route
-              path="/portal/patient/medical-reports/:id"
-              element={
-                <div>
-                  <h1 className="text-center text-3xl font-bold">
-                    Report Details
-                  </h1>
-                  <p>Report Details</p>
-                </div>
-              }
-            />
+      <main className="hidden md:block">
+        <Routes>
+          <Route index element={<LandingPage />}></Route>
+          <Route path="/auth" element={<Auth />}></Route>
+          <Route path="/dashboard/patient" element={<PatientDashboard />}>
             <Route path="chatbot/:sessionId" element={<Chatbot />} />
             <Route path="chatbot" element={<Chatbot />} />
-            <Route path="reports" element={<PatientReportDetailsPage />}>
-              <Route
-                path="/portal/patient/reports/:id"
-                element={<MedicalReportDetails />}
-              />
-            </Route>
+            <Route path="reports" element={<Reports />} />
             <Route
-              path="/portal/patient"
-              element={<Navigate to="/portal/patient/reports" />}
+              path="/dashboard/patient"
+              element={<Navigate to="/dashboard/patient/chatbot" />}
             />
           </Route>
 
-          <Route path="/portal/doctor" element={<DoctorPortal />}>
-            <Route
-              path="/portal/doctor/overview"
-              element={<DoctorDashboard />}
-            />
-            <Route
-              path="/portal/doctor/appointments"
-              element={<AppointmentsPage />}
-            />
-            <Route
-              path="/portal/doctor/medical-reports/:id"
-              element={<MedicalReportDetails />}
-            />
-            <Route
-              path="/portal/doctor/medical-reports"
-              element={<MedicalReports />}
-            />
-            <Route path="/portal/doctor/patients" element={<PatientsPage />} />
-            <Route
-              path="/portal/doctor/medical-reports/new"
-              element={<NewMedicalReportPage />}
-            />
-            <Route
-              path="/portal/doctor"
-              element={<Navigate to={'/portal/doctor/overview'} />}
-            />
+          <Route path="/doctor/onboarding" element={<DoctorOnboardingPage />} />
+          <Route path="/onboarding/doctor" element={<DoctorOnboardingPage />} />
+          <Route
+            path="/onboarding/patient"
+            element={<PatientOnboardingPage />}
+          />
+          <Route path="/register/doctor" element={<SignUpPage />} />
+          <Route path="/register/patient" element={<SignUpPage />} />
+          <Route path="/login/doctor" element={<LoginPage />} />
+          <Route path="/login/patient" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/portal" element={<PortalContainer />}>
+            <Route path="/portal/patient" element={<PatientDashboard />}>
+              <Route
+                path="/portal/patient/medical-reports/:id"
+                element={
+                  <div>
+                    <h1 className="text-center text-3xl font-bold">
+                      Report Details
+                    </h1>
+                    <p>Report Details</p>
+                  </div>
+                }
+              />
+              <Route path="chatbot/:sessionId" element={<Chatbot />} />
+              <Route path="chatbot" element={<Chatbot />} />
+              <Route path="reports" element={<PatientReportDetailsPage />}>
+                <Route
+                  path="/portal/patient/reports/:id"
+                  element={<MedicalReportDetails />}
+                />
+              </Route>
+              <Route
+                path="/portal/patient"
+                element={<Navigate to="/portal/patient/reports" />}
+              />
+            </Route>
+
+            <Route path="/portal/doctor" element={<DoctorPortal />}>
+              <Route
+                path="/portal/doctor/overview"
+                element={<DoctorDashboard />}
+              />
+              <Route
+                path="/portal/doctor/appointments"
+                element={<AppointmentsPage />}
+              />
+              <Route
+                path="/portal/doctor/medical-reports/:id"
+                element={<MedicalReportDetails />}
+              />
+              <Route
+                path="/portal/doctor/medical-reports"
+                element={<MedicalReports />}
+              />
+              <Route
+                path="/portal/doctor/patients"
+                element={<PatientsPage />}
+              />
+              <Route
+                path="/portal/doctor/medical-reports/new"
+                element={<NewMedicalReportPage />}
+              />
+              <Route
+                path="/portal/doctor"
+                element={<Navigate to={'/portal/doctor/overview'} />}
+              />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
-    </main>
+        </Routes>
+      </main>
+    </>
   );
 };
 
---
+++ frontend/src/components/MaintenanceScreen.tsx
@@ -0,0 +1,35 @@
+import Lottie from 'react-lottie';
+import maintenanceAnimation from '../assets/animations/maintenace_animation.json';
+import logo from '../assets/images/logo.png';
+
+const MaintenanceScreen = () => {
+  return (
+    <div className="flex md:hidden py-20 flex-col gap-y-5  justify-center items-center">
+      <div>
+        <div>
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: maintenanceAnimation,
+              rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice',
+              },
+            }}
+            height={150}
+            width={150}
+          />
+        </div>
+        <div className="flex items-center container max-w-xs">
+          <img className="h-12 w-auto" src={logo} alt="Heart AI Logo" />
+          <p className="text-xs py-10 px-5">
+            Sorry, this site is being optimized for mobile views. Please try
+            viewing them on a larger screen.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MaintenanceScreen;
